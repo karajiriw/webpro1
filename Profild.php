95e6c3fbbc00bc57ee03a4733112f2e88d6b7284
karajiriw <152352200+karajiriw@users.noreply.github.com>
2023-12-19 21:12:50 +0700

    Add files via upload

---
+++ Profild.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="Pro.css">
+</head>
+<body>
+    <?php
+    $name = "นายกฤติพงศ์ ศรีสุข";
+    $nname = "โกกิ";
+    $pass = "6506021621195";
+    $free = "ร้องเพลงกับเล่นเกม";
+    $phon = "094-7931895";
+    ?>
+    <div>
+        <div class="Kmutnb"><img src="Seal_of_KMUTNB.svg.png" width="300px" height="300px"></div>
+        <div><h4 id="name">ชื่อ-นามสกุล</h4><?php echo($name) ?></div>
+        <div class="nickname"><h4>ชื่อเล่น</h4><?php echo($nname) ?></div>
+        <div class="pass"><h4>รหัสนักศึกษา</h4><?php echo($pass) ?></div>
+        <div class="free"><h4>งานอดิเรก</h4><?php echo($free) ?></div>
+        <div class="phon"><h4>เบอร์ติดต่อ</h4><?php echo($phon) ?></div>
+        <div class="facebook"><a href="https://www.facebook.com/profile.php?id=100011715606358"><img src="2021_Facebook_icon.svg.png" width="100px" height="100px"></a></div>
+        <div><a href="Homepage.html"><i class="fa-solid fa-reply-all"></i></a></div>
+        <div><a href="https://github.com/karajiriw"><p>https://github.com/karajiriw</p></a></div>
+    </div>
+</body>
+</html>
